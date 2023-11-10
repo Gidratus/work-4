e92843d34125b03b8d6c6630468286e64210309f
Gidratus <150123665+Gidratus@users.noreply.github.com>
2023-11-10 14:17:12 +0300

    НОВАЯ 15 работа

--- Работа 15 (Рубцов Юра)/Работа 15 (Рубцов Юра)/Работа 15 (Рубцов Юра).cpp
+++ Работа 15 (Рубцов Юра)/Работа 15 (Рубцов Юра)/Работа 15 (Рубцов Юра).cpp
@@ -2,21 +2,93 @@
 //
 
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-    setlocale(0, ""); // Перевод
-    int n;
-    float x, v, s;
-    cout << "Сумма вклада: ";
-    cin >> s;  // первое значение
-    cout << "Количество лет: ";
-    cin >> n;  // второе значение
-    x = s * 3 / 100;  // решение
-    v = s + x * n;
-    cout << "Сумма через " << n << " лет: " << v << endl; //ответ
-    return 0;
+	setlocale(0, "rus");
+	float value;
+
+	cout << "Выберите вариант перевода: \n";
+	cout << "1. Сажени в метры\n";
+	cout << "2. Дюймы в сантиметры\n";
+	cout << "3. Футы в метры\n";
+	cout << "4. Драхмы в граммы\n";
+	cout << "5. Унции в граммы\n";
+	cout << "6. Фунты в килограммы\n";
+	cout << "7. Аршины в метры\n";
+	cout << "8. Золотники в граммы\n";
+	cout << "9. Дюймы в миллиметры\n";
+
+	int option;
+	cout << "Ваш выбор: ";
+	cin >> option;
+
+	switch (option)
+	{
+	case 1:
+		cout << "Введите значение в саженях: ";
+		cin >> value;
+		cout << value << " сажень(ей) = " << value * 2.1366 << " м\n";
+		break;
+
+	case 2:
+		cout << "Введите значение в дюймах: ";
+		cin >> value;
+		cout << value << " дюйм(ов) = " << value * 2.54 << " см\n";
+		break;
+
+	case 3:
+		cout << "Введите значение в футах: ";
+		cin >> value;
+		cout << value << " фут(ов) = " << value * 0.3048 << " м\n";
+		break;
+
+	case 4:
+		cout << "Введите значение в драхмах: ";
+		cin >> value;
+		cout << value << " драхма(ы) = " << value * 3.7325 << " г\n";
+		break;
+
+	case 5:
+		cout << "Введите значение в унциях: ";
+		cin >> value;
+		cout << value << " унция(и) = " << value * 29.86 << " г\n";
+		break;
+
+	case 6:
+		cout << "Введите значение в фунтах: ";
+		cin >> value;
+		cout << value << " фунт(ов) = " << value * 0.40951 << " кг\n";
+		break;
+
+	case 7:
+		cout << "Введите значение в аршинах: ";
+		cin >> value;
+		cout << value << " аршин(ов) = " << value * 0.7112 << " м\n";
+		break;
+
+	case 8:
+		cout << "Введите значение в золотниках: ";
+		cin >> value;
+		cout << value << " золотник(ов) = " << value * 4.2657 << " г\n";
+		break;
+
+	case 9:
+		cout << "Введите значение в дюймах: ";
+		cin >> value;
+		cout << value << " дюйм(ов) = " << value * 25.3995 << " мм\n";
+		break;
+
+	default:
+		cout << "Некорректный вариант перевода\n";
+		break;
+	}
+
+	return 0;
 }
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
